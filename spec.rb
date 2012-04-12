44dbdac5ad95757173af57cc7a66f953c994799d
Łukasz Bandzarewicz <lucassus@gmail.com>
2012-04-12 17:19:56 +0200

    Added instance_eval.

--- spec.rb
+++ spec.rb
@@ -1,7 +1,18 @@
 def describe(description, &block)
-  block.call
+  ExampleGroup.new(block).evaluate!
 end
 
-def it(description, &block)
-  block.call
+class ExampleGroup
+  def initialize(block)
+    @block = block
+  end
+
+  def evaluate!
+    instance_eval(&@block)
+  end
+
+  def it(description, &block)
+    block.call
+  end
 end
+
