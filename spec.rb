a07bfeb66f3ff01f649172c64a6fc2b6958a30d1
Łukasz Bandzarewicz <lucassus@gmail.com>
2012-04-12 17:25:47 +0200

    Add basic should method.

--- spec.rb
+++ spec.rb
@@ -16,3 +16,21 @@ class ExampleGroup
   end
 end
 
+class Object
+  def should
+    DelayedAssertion.new(self)
+  end
+end
+
+class AssertionError < Exception; end
+
+class DelayedAssertion
+  def initialize(subject)
+    @subject = subject
+  end
+
+  def ==(other)
+    raise AssertionError unless @subject == other
+  end
+end
+
--- test/test_spec.rb
+++ test/test_spec.rb
@@ -24,5 +24,11 @@ class TestAssertions < Test::Unit::TestCase
   def test_it_can_pass
     2.should == 2
   end
+
+  def test_it_can_fail
+    assert_raise(AssertionError) do
+      1.should == 2
+    end
+  end
 end
 
