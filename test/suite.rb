db983c108a00b5fa0675baf0b7905b1d8348021f
Łukasz Bandzarewicz <lucassus@gmail.com>
2012-04-16 16:41:14 +0200

    Dry loading test files.

--- test/suite.rb
+++ test/suite.rb
@@ -1,9 +1,6 @@
 $: << File.expand_path('../..', __FILE__)
 require 'test/helper'
 
-require 'test/test_assertions'
-require 'test/test_before'
-require 'test/test_describe'
-require 'test/test_let'
-require 'test/test_matchers'
-require 'test/test_subject'
+Dir['test/test_*.rb'].each do |test_file|
+  require test_file
+end
